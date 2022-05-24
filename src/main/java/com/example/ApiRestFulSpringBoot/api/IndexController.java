b26a62c9c19a1c5af9e706c5510329478da9dbaa
Bruna Alves <bu.alves.azevedo@gmail.com>
2022-05-24 14:46:10 -0300

    Ultima atualizacao

--- src/main/java/com/example/ApiRestFulSpringBoot/api/IndexController.java
+++ src/main/java/com/example/ApiRestFulSpringBoot/api/IndexController.java
@@ -1,5 +1,20 @@
 package com.example.ApiRestFulSpringBoot.api;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
 public class IndexController {
-    
+
+    @GetMapping()
+    public String get()
+    {
+        return "Get Spring Boot";
+    }
+
+    @PostMapping("/login")
+    public String login (@RequestParam("usuario") String usuario, @RequestParam("senha") String senha)
+    {
+        return "Usuario " + usuario + ", senha " + senha;
+    }
 }
